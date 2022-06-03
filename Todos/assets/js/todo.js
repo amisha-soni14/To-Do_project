1ececd9c5d4fb4389296e3367f7903024e60a28b
amisha_soni14 <amishas@shriffle.com>
2022-06-03 22:43:00 +0530

    Dynamic list creation and update each list element

--- Todos/assets/js/todo.js
+++ Todos/assets/js/todo.js
@@ -1,62 +1,60 @@
-var addButton, data = [], statement, obj, listItem, getData;
+let addButton, listItem, todoArray = [], liData;
 
-function todo() {
-  addButton = document.getElementById('todo').value;
-  console.log(addButton)
-  addData(addButton);
-}
-function addData(statement) {
-  let today = new Date();
-  let date = today.getDate()+' - '+(today.toLocaleString('default', { month: 'short' }) )+' - '+today.getFullYear();
-  console.log(date);
-  obj = {
-    text : statement,
-    id : date
+function addTodo() {
+  const today = new Date();
+  const date = `${today.getDate()} - ${(today.toLocaleString('default', { month: 'short' }) )} - ${today.getFullYear()}`;
+  const todoObj = {
+    id: Date.now(),
+    text: document.getElementById('todo').value,
+    date: date
   };
-  data.push(obj);
-  localStorage.setItem("value", JSON.stringify(data));
-  var retrievedData = localStorage.getItem("value");
-  getData = JSON.parse(retrievedData);
-  console.log(getData);
-  displayData();
+
+  todoArray.push(todoObj);
+  localStorage.setItem("todos", JSON.stringify(todoArray));
+  displayData(todoObj);
   reset();
 }
 
-function displayData() {
-  listItem = getData.map(function(element) {
-    console.log(`${element.text}`)
-    return `${element.text}`;
-  })
-  let createDate = getData.map(function(ele) {
-    console.log(`${ele.id}`)
-    return `${ele.id}`;
-  })
-  document.getElementById("display").innerHTML = listItem;
-  document.getElementById("dis-date").innerHTML = `Created at : ${createDate}`;
+function displayData({id, text, date}) {
+  const li = `<li id="${ id }">${text} ${date}
+      <button class="btn btn-info" onclick="editTask('${id}', '${text}');" aria-label="Edit">
+        <i class="fas fa-edit" aria-hidden="true"></i>
+      </button>
+      <button class="btn btn-danger" onclick="deleteTask('${id}', '${text}');">
+        <i class="fas fa-trash-alt" aria-hidden="true"></i>
+      </button>
+    </li>`;
+
+  document.getElementById('task-data').insertAdjacentHTML('beforeend', li);
 }
 
 function reset() {
-  document.getElementById("todo").innerHTML = "  ";
+  document.getElementById("todo").value = "";
 }
 
-function editTask() {
-  let edit = prompt("Edit Your Text !!", `${listItem}`);
-  let editData = getData.findIndex((obj => obj.text = edit));
-  getData[editData].text = edit;
-  localStorage.setItem("value", JSON.stringify(getData));
-  let updateData = getData.map(function(element) {
-    return `${element.text}`;
-  })
-  document.getElementById("display").innerHTML = updateData;
+function editTask(id, text) {
+  const edit = prompt("Edit Your Text !!", `${text}`);
+
+  if(edit) {
+    todoArray.map((todo) => {
+      if(todo.id === +id) {
+        todo.text = edit;
+      }
+      return todo;
+    });
+    localStorage.setItem("todos", JSON.stringify(todoArray));
+    console.log(todoArray)
+  }
+
 }
 
-function deleteTask() {
-  let removedEl = getData.pop();
-  console.log(removedEl);
-  localStorage.setItem("value", JSON.stringify(getData));
-  let removeData = getData.map(function(element) {
-    return `${element.text}${element.id}`;
-  })
-  document.getElementById("task-data").innerHTML = removeData;
-}
+// function deleteTask() {
+//   let removedEl = todoArray.pop();
+//   console.log(removedEl);
+//   localStorage.setItem("todos", JSON.stringify(todoArray));
+//   let removeData = todoArray.map(function(element) {
+//     return `${element.text}${element.date}`;
+//   })
+//   document.getElementById("task-data").innerHTML = removeData;
+// }
 
