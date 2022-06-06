1ac9b0e17515dc3e5e4d252eabb044a4b49a34d4
amisha_soni14 <amishas@shriffle.com>
2022-06-06 23:19:50 +0530

    Add and update in dynamic list of todo

--- Todos/assets/js/todo.js
+++ Todos/assets/js/todo.js
@@ -16,14 +16,16 @@ function addTodo() {
 }
 
 function displayData({id, text, date}) {
-  const li = `<li id="${ id }">${text} ${date}
-      <button class="btn btn-info" onclick="editTask('${id}', '${text}');" aria-label="Edit">
-        <i class="fas fa-edit" aria-hidden="true"></i>
-      </button>
-      <button class="btn btn-danger" onclick="deleteTask('${id}', '${text}');">
-        <i class="fas fa-trash-alt" aria-hidden="true"></i>
-      </button>
-    </li>`;
+  const li = `<li id="${ id }">
+    <span id="span1">${text}</span>
+    ${date}
+    <button class="btn btn-info" id="edit" onclick="editTask('${id}', '${text}' ,'${date}');" aria-label="Edit">
+      <i class="fas fa-edit" aria-hidden="true"></i>
+    </button>
+    <button class="btn btn-danger" id="delete" onclick="deleteTask('${id}');">
+      <i class="fas fa-trash-alt" aria-hidden="true"></i>
+    </button>
+  </li>`;
 
   document.getElementById('task-data').insertAdjacentHTML('beforeend', li);
 }
@@ -32,7 +34,7 @@ function reset() {
   document.getElementById("todo").value = "";
 }
 
-function editTask(id, text) {
+function editTask(id, text, date) {
   const edit = prompt("Edit Your Text !!", `${text}`);
 
   if(edit) {
@@ -44,12 +46,28 @@ function editTask(id, text) {
     });
     localStorage.setItem("todos", JSON.stringify(todoArray));
     console.log(todoArray)
+    todoArray.map((e) => {
+      if(e.id === +id) {
+        let updateData = document.getElementById(id).querySelector('span').innerText;
+        updateData = edit;
+        document.getElementById(id).querySelector('span').innerText = updateData;
+      }
+    });
   }
-
 }
 
-// function deleteTask() {
-//   let removedEl = todoArray.pop();
+
+
+
+function deleteTask(id) {
+debugger
+  //const deleteId = id;
+  deleteData = todoArray.filter((e) => (e.id !== +id));
+      // todoArray.splice(0,todoArray.findIndex(deleteId));
+  console.log(deleteData)
+  localStorage.setItem("todos", JSON.stringify(deleteData));
+}
+//
 //   console.log(removedEl);
 //   localStorage.setItem("todos", JSON.stringify(todoArray));
 //   let removeData = todoArray.map(function(element) {
